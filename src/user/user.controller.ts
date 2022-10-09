3a6fe97f0b6938088a0712d08a49b309a56dbca6
mriusdev <93098512+mriusdev@users.noreply.github.com>
2022-10-09 22:30:12 +0300

    add find user by username route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, HttpCode, HttpStatus, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { JwtAuthAccessGuard } from '../auth/guard';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
-  
+  constructor(private userService: UserService) {}
+
   @UseGuards(JwtAuthAccessGuard)
   @HttpCode(HttpStatus.OK)
   @Get('me')
   getMe(@Req() req: Request) {
     return req.user;
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':username')
+  getUser(@Param('username') username: string) {
+    console.log(username);
+    return this.userService.getUser(username)
   }
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { NotFoundError } from '@prisma/client/runtime';
+import { GenericException } from '../common/helpers/exceptions';
+import { GenericSuccessResponse } from '../common/helpers/responses';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  async getUser(username: string) {
+    console.log(username);
+    try {
+      const user = await this.prisma.user.findUniqueOrThrow({
+        where: {
+          username
+        },
+        select: {
+          username: true,
+          name: true
+        }
+      })
+      return GenericSuccessResponse(undefined, undefined, user)
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw new NotFoundException()
+      }
+      throw new GenericException();
+    }
+  }
+}
