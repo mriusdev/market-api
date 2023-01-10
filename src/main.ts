a2cd14ed720ba37a4a8b4ad0e5c626ef01577993
mriusdev <93098512+mriusdev@users.noreply.github.com>
2023-01-10 21:37:48 +0200

    add env for global port

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
-  await app.listen(3000);
+  await app.listen(config.get('APP_PORT') || 3000);
 }
 bootstrap();
